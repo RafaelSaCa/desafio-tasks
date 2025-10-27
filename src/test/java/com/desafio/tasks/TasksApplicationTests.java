a5cc579adaa498541a45c526cd50807ffacab895
RafaelSaCa <raffaellsaca@gmail.com>
2025-10-26 21:58:05 -0300

    commit inicial

---
+++ src/test/java/com/desafio/tasks/TasksApplicationTests.java
@@ -0,0 +1,13 @@
+package com.desafio.tasks;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TasksApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
