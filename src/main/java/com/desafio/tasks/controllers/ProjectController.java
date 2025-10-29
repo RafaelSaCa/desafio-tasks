f9d803bfdadf2f2df2465d9cb5eaa4b6d9ae9b75
RafaelSaCa <raffaellsaca@gmail.com>
2025-10-29 13:24:34 -0300

    controller project - findAll

--- src/main/java/com/desafio/tasks/controllers/ProjectController.java
+++ src/main/java/com/desafio/tasks/controllers/ProjectController.java
@@ -1,8 +1,11 @@
 package com.desafio.tasks.controllers;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -16,13 +19,19 @@ import com.desafio.tasks.services.ProjectService;
 @RequestMapping("/projects")
 public class ProjectController {
 
-    @Autowired
-    private ProjectService service;
+        @Autowired
+        private ProjectService service;
 
-    @PostMapping
-    public ResponseEntity<Project> create (@RequestBody ProjectDto request){
-        Project project = service.create(request);
-        return ResponseEntity.status(HttpStatus.CREATED).body(project);
-    }
+        @PostMapping
+        public ResponseEntity<Project> create(@RequestBody ProjectDto request) {
+                Project project = service.create(request);
+                return ResponseEntity.status(HttpStatus.CREATED).body(project);
+        }
 
+        @GetMapping
+        public ResponseEntity<List<Project>> listAll() {
+                List<Project> projects = service.listAll();
+                return ResponseEntity.ok(projects);
+
+        }
 }
