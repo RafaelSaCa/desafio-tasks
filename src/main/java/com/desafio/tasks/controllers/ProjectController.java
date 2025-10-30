214fe9d1019265748a095be8f4c598db6820adad
RafaelSaCa <raffaellsaca@gmail.com>
2025-10-29 21:53:58 -0300

    controller project - delete

--- src/main/java/com/desafio/tasks/controllers/ProjectController.java
+++ src/main/java/com/desafio/tasks/controllers/ProjectController.java
@@ -5,7 +5,9 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -32,6 +34,11 @@ public class ProjectController {
         public ResponseEntity<List<Project>> listAll() {
                 List<Project> projects = service.listAll();
                 return ResponseEntity.ok(projects);
+        }
 
+        @DeleteMapping("/{id}")
+        public ResponseEntity<Void> delete(@PathVariable Long id ){
+                service.delete(id);
+                return ResponseEntity.noContent().build();
         }
 }
